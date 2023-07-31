c087520823e85a11c3712e5c7bda74bdd1b0c281
jeriatno <jeriat01@gmail.com>
2023-07-31 20:40:53 +0800

    Initial commit

---
+++ components/admin_page/admin_tab.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import InvoiceForm from "./invoice_tab/InvoiceForm";
+import AddService from "./service_tab/add_service";
+import Navigation from "../layout/navigation";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function AdminTab() {
+  const [value, setValue] = React.useState(0);
+  const auth = getAuth();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.push("/");
+        toast("Anda Harus Login Terlebih Dahulu !");
+      }
+    });
+  }, []);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Navigation />
+      <div className="max-w-[1200px] mx-auto">
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs value={value} onChange={handleChange}>
+              <Tab label="Print Invoice" {...a11yProps(0)} />
+              <Tab label="Tambah Jasa Sewa" {...a11yProps(1)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+            <InvoiceForm />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <AddService />
+          </TabPanel>
+        </Box>
+      </div>
+    </div>
+  );
+}
---
+++ components/admin_page/invoice_tab/InvoiceForm.tsx
@@ -0,0 +1,276 @@
+"use client";
+
+import { useState } from "react";
+import { uid } from "uid";
+import InvoiceItem from "./InvoiceItem";
+import InvoiceModal from "./InvoiceModal";
+import { Button, TextField } from "@mui/material";
+
+const date = new Date();
+const today = date.toLocaleDateString("en-GB", {
+  month: "numeric",
+  day: "numeric",
+  year: "numeric",
+});
+
+const InvoiceForm = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [invoiceNumber, setInvoiceNumber] = useState(1);
+  const [telahTerimaDari, setTelahTerimaDari] = useState("");
+  const [uangSejumlah, setUangSejumlah] = useState("");
+  const [items, setItems] = useState([
+    {
+      id: uid(6),
+      uraian: "",
+      qty: 1,
+      jenisTruk: "CDD",
+      kota: "Jakarta - Bandung",
+      jarakKota: 0,
+      qtyBags: 0,
+      kg: 0,
+      harga: 0,
+      keterangan: "",
+    },
+  ]);
+
+  const reviewInvoiceHandler = (event: any) => {
+    event.preventDefault();
+    setIsOpen(true);
+  };
+
+  const addNextInvoiceHandler = () => {
+    setInvoiceNumber((prevNumber) => prevNumber);
+    setItems([
+      {
+        id: uid(6),
+        uraian: "",
+        qty: 1,
+        jenisTruk: "CDD",
+        kota: "Jakarta - Bandung",
+        jarakKota: 0,
+        qtyBags: 0,
+        kg: 0,
+        harga: 0,
+        keterangan: "",
+      },
+    ]);
+  };
+
+  const addItemHandler = () => {
+    const id = uid(6);
+    setItems((prevItem) => [
+      ...prevItem,
+      {
+        id: uid(6),
+        uraian: "",
+        qty: 1,
+        jenisTruk: "CDD",
+        kota: "Jakarta - Bandung",
+        jarakKota: 0,
+        qtyBags: 0,
+        kg: 0,
+        harga: 0,
+        keterangan: "",
+      },
+    ]);
+  };
+
+  const deleteItemHandler = (id: any) => {
+    setItems((prevItem) => prevItem.filter((item) => item.id !== id));
+  };
+
+  const edtiItemHandler = (event: any) => {
+    const editedItem = {
+      id: event.target.id,
+      name: event.target.name,
+      value: event.target.value,
+    };
+
+    const newItems = items.map((items: any) => {
+      for (const key in items) {
+        if (key === editedItem.name && items.id === editedItem.id) {
+          items[key] = editedItem.value;
+          items["harga"] = calculatePrice(items["kg"], items["jarakKota"]);
+        }
+      }
+      return items;
+    });
+
+    setItems(newItems);
+  };
+
+  const calculatePrice = (bebanMuatan: number, distance: number) => {
+    let rate = 0;
+    if (distance >= 1 && distance <= 10) {
+      rate = 91;
+    } else if (distance > 10 && distance <= 20) {
+      rate = 118;
+    } else if (distance > 20 && distance <= 30) {
+      rate = 148;
+    } else if (distance > 30 && distance <= 40) {
+      rate = 167;
+    } else if (distance > 40 && distance <= 100) {
+      rate = 260;
+    } else if (distance > 100 && distance <= 200) {
+      rate = 320;
+    } else if (distance > 200 && distance <= 300) {
+      rate = 400;
+    } else if (distance > 300 && distance <= 400) {
+      rate = 550;
+    } else if (distance > 400 && distance <= 500) {
+      rate = 675;
+    }
+
+    return rate * bebanMuatan;
+  };
+
+  const total = items.reduce((total, num) => {
+    return total + Number(num.harga);
+  }, 0);
+
+  return (
+    <div>
+      <form onSubmit={reviewInvoiceHandler}>
+        <div className="my-6 rounded-md bg-white p-4 border space-y-8 md:p-6">
+          <div className="flex flex-col justify-between border-b border-gray-900/10 pb-4 md:flex-row md:items-center">
+            <div className="flex space-x-2">
+              <span className="font-bold">Tanggal Hari Ini : </span>
+              <span>{today}</span>
+            </div>
+            <div className="flex items-center space-x-2">
+              <label className="font-bold" htmlFor="invoiceNumber">
+                Kode Invoice :
+              </label>
+              <input
+                required
+                className="max-w-[150px] border p-1 outline-none"
+                type="number"
+                name="invoiceNumber"
+                id="invoiceNumber"
+                min="1"
+                step="1"
+                value={invoiceNumber}
+                onChange={(event) =>
+                  setInvoiceNumber(event.target.value as any)
+                }
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center space-x-2">
+            <div className="text-sm font-bold sm:text-base w-[150px]">
+              Telah Terima Dari :
+            </div>
+            <TextField
+              required
+              sx={{ width: "fit-content" }}
+              label="Telah Terima Dari"
+              type="text"
+              name="telahTerimaDari"
+              id="telahTerimaDari"
+              value={telahTerimaDari}
+              onChange={(event) => setTelahTerimaDari(event.target.value)}
+            />
+          </div>
+
+          <div className="flex items-center space-x-2">
+            <div className="text-sm font-bold sm:text-base w-[150px]">
+              Uang sejumlah :
+            </div>
+            <TextField
+              required
+              sx={{ width: "fit-content" }}
+              label="Uang Sejumlah"
+              type="text"
+              name="uangSejumlah"
+              id="uangSejumlah"
+              value={uangSejumlah}
+              onChange={(event) => setUangSejumlah(event.target.value)}
+            />
+          </div>
+
+          <div className="w-full overflow-x-scroll">
+            <table className="w-full p-4 text-left">
+              <thead>
+                <tr className="border-b border-gray-900/10 text-sm md:text-base">
+                  <th>No</th>
+                  <th>Uraian</th>
+                  <th>Jenis Truk</th>
+                  <th>Kota</th>
+                  <th>Jarak ( Dalam KM )</th>
+                  <th>QTY Bags</th>
+                  <th>Kg</th>
+                  <th>Harga</th>
+                  <th>Keterangan</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {items.map((item, index) => (
+                  <InvoiceItem
+                    index={index}
+                    key={item.id}
+                    id={item.id}
+                    uraian={item.uraian}
+                    jenisTruk={item.jenisTruk}
+                    kota={item.kota}
+                    jarakKota={item.jarakKota}
+                    qtyBags={item.qtyBags}
+                    kg={item.kg}
+                    harga={item.harga}
+                    keterangan={item.keterangan}
+                    onDeleteItem={deleteItemHandler}
+                    onEditItem={edtiItemHandler}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <Button
+            onClick={addItemHandler}
+            variant="contained"
+            className="bg-primary"
+            sx={{ color: "white" }}
+          >
+            Tambahkan Item
+          </Button>
+          {/* Total  */}
+          <div className="flex flex-col space-y-2 mt-5 w-full">
+            <div className="flex w-full justify-between pt-2 md:w-1/2">
+              <span className="font-bold">Total Harga :</span>
+              <span className="font-bold">
+                {total.toLocaleString().replaceAll(",", ".")}
+              </span>
+            </div>
+          </div>
+          <Button
+            type="submit"
+            fullWidth={true}
+            variant="contained"
+            className="bg-primary"
+            sx={{ color: "white" }}
+          >
+            Review Invoice
+          </Button>
+        </div>
+
+        <div>
+          <InvoiceModal
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            invoiceInfo={{
+              invoiceNumber,
+              total,
+              telahTerimaDari,
+              uangSejumlah,
+            }}
+            items={items}
+            onAddNextInvoice={addNextInvoiceHandler}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default InvoiceForm;
